ddbec4c03c33e8a99e11c29f1394268820800746
Anirudh Kauta <kanirudh993@gmail.com>
2018-07-18 23:29:52 -0500

    PEP8 formatting

--- test_thermo.py
+++ test_thermo.py
@@ -42,15 +42,17 @@ operating_mode = 'Fire'
 high_temp = 200
 low_temp = 0
 
+
 class TestIndex():
 
     def test_GET(self):
-        # self.assertEqual(thermo.Index().GET(),200)
+        # self.assertEqual(thermo.Index().GET(), 200)
         middleware = []
         testApp = TestApp(app.wsgifunc(*middleware))
         r = testApp.get('/')
         assert_equal(r.status, 200)
         r.mustcontain('200 OK')
+
 
 class TestThermostat():
 
@@ -58,26 +60,26 @@ class TestThermostat():
         middleware = []
         testApp = TestApp(app.wsgifunc(*middleware))
 
-        #Return all thermostats with 200 OK
-        r = testApp.get('/thermostats',{'content-type':'application/json'})
+        # Return all thermostats with 200 OK
+        r = testApp.get('/thermostats', {'content-type': 'application/json'})
         json_data = json.loads(r.body)
         assert_equal(r.status, 200)
-        assert_equal(json_data,thermostats)
+        assert_equal(json_data, thermostats)
 
-        #Return thermostat with id 100 with 200 OK
-        r = testApp.get('/thermostats/100',{'content-type':'application/json'})
+        # Return thermostat with id 100 with 200 OK
+        r = testApp.get('/thermostats/100', {'content-type': 'application/json'})
         json_data = json.loads(r.body)
         assert_equal(r.status, 200)
-        assert_equal(json_data,thermostats[0])
+        assert_equal(json_data, thermostats[0])
 
-        #Return 404 error for thermostat that doesn't exists
-        r = testApp.get('/thermostats/102',status="*")
+        # Return 404 error for thermostat that doesn't exists
+        r = testApp.get('/thermostats/102', status="*")
         assert_equal(r.status, 404)
         r.mustcontain('not found')
 
-        #This test case is failing as non int ids are not captured from the URL
-        #Return 406 notacceptable for thermostat id that isn't int
-        # r = testApp.get('/thermostats/abc',status="*")
+        # This test case is failing as non int ids are not captured from the URL
+        # Return 406 notacceptable for thermostat id that isn't int
+        # r = testApp.get('/thermostats/abc', status="*")
         # assert_equal(r.status, 406)
         # r.mustcontain('not acceptable')
 
@@ -85,48 +87,48 @@ class TestThermostat():
         middleware = []
         testApp = TestApp(app.wsgifunc(*middleware))
 
-        #Return 200 OK for putting a valid setting
-        r = testApp.put('/thermostats/100/cool_setPoint',params=json.dumps(cool_setPoint))
+        # Return 200 OK for putting a valid setting
+        r = testApp.put('/thermostats/100/cool_setPoint', params=json.dumps(cool_setPoint))
         json_data = json.loads(r.body)
-        assert_equal(r.status,200)
-        assert_equal(json_data,thermostat_cool_set)
+        assert_equal(r.status, 200)
+        assert_equal(json_data, thermostat_cool_set)
 
-        #Return 403 forbidden for assigning string for int value
-        r = testApp.put('/thermostats/100/id',params=json.dumps(cool_setPoint_string),status="*")
-        assert_equal(r.status,403)
+        # Return 403 forbidden for assigning string for int value
+        r = testApp.put('/thermostats/100/id', params=json.dumps(cool_setPoint_string), status="*")
+        assert_equal(r.status, 403)
         r.mustcontain('forbidden')
 
-        #Return 403 forbidden for read only setting
-        r = testApp.put('/thermostats/100/id',params=json.dumps(new_id),status="*")
-        assert_equal(r.status,403)
+        # Return 403 forbidden for read only setting
+        r = testApp.put('/thermostats/100/id', params=json.dumps(new_id), status="*")
+        assert_equal(r.status, 403)
         r.mustcontain('forbidden')
 
-        #Return 400 bad request if put is attempted without a specific setting
-        r = testApp.put('/thermostats/100',params=json.dumps(new_id),status="*")
-        assert_equal(r.status,400)
+        # Return 400 bad request if put is attempted without a specific setting
+        r = testApp.put('/thermostats/100', params=json.dumps(new_id), status="*")
+        assert_equal(r.status, 400)
         r.mustcontain('bad request')
 
-        #Return 400 bad request for non-existent setting
-        r = testApp.put('/thermostats/100/clock_display',params=json.dumps(new_temp),status="*")
-        assert_equal(r.status,400)
+        # Return 400 bad request for non-existent setting
+        r = testApp.put('/thermostats/100/clock_display', params=json.dumps(new_temp), status="*")
+        assert_equal(r.status, 400)
         r.mustcontain('bad request')
 
-        #Return 400 bad request for setting values higher than bounds
-        r = testApp.put('/thermostats/100/cool_setPoint',params=json.dumps(high_temp),status="*")
-        assert_equal(r.status,400)
+        # Return 400 bad request for setting values higher than bounds
+        r = testApp.put('/thermostats/100/cool_setPoint', params=json.dumps(high_temp), status="*")
+        assert_equal(r.status, 400)
         r.mustcontain('bad request')
 
-        #Return 400 bad request for setting values lower than bounds
-        r = testApp.put('/thermostats/100/cool_setPoint',params=json.dumps(low_temp),status="*")
-        assert_equal(r.status,400)
+        # Return 400 bad request for setting values lower than bounds
+        r = testApp.put('/thermostats/100/cool_setPoint', params=json.dumps(low_temp), status="*")
+        assert_equal(r.status, 400)
         r.mustcontain('bad request')
 
-        #Return 400 bad request for setting values out of scope
-        r = testApp.put('/thermostats/100/operating_mode',params=json.dumps(operating_mode),status="*")
-        assert_equal(r.status,400)
+        # Return 400 bad request for setting values out of scope
+        r = testApp.put('/thermostats/100/operating_mode', params=json.dumps(operating_mode), status="*")
+        assert_equal(r.status, 400)
         r.mustcontain('bad request')
 
-        #Return 404 for non-existent thermostat
-        r = testApp.put('/thermostats/108/cool_setPoint',params=json.dumps(cool_setPoint),status="*")
-        assert_equal(r.status,404)
+        # Return 404 for non-existent thermostat
+        r = testApp.put('/thermostats/108/cool_setPoint', params=json.dumps(cool_setPoint), status="*")
+        assert_equal(r.status, 404)
         r.mustcontain('not found')
--- thermo.py
+++ thermo.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python
+# !/usr/bin/python
 import web
 import json
 
-#The routes for the api calls
+# The routes for the api calls
 urls = (
-    '/?', 'Index',
-    '/thermostats/(\d+)/(\w+)/?', 'Thermostat',
-    '/thermostats/?(\d+)?/?', 'Thermostat',
-    '/thermostats/?', 'Thermostat',
+    '/?',  'Index',
+    '/thermostats/(\d+)/(\w+)/?',  'Thermostat',
+    '/thermostats/?(\d+)?/?',  'Thermostat',
+    '/thermostats/?',  'Thermostat',
 )
 
-#Thermostat items
+# Thermostat items
 thermostats = [
     {
         "id": 100,
@@ -32,24 +32,25 @@ thermostats = [
     }
 ]
 
-#Thermostat settings
-thermo_props = {"id","name","operating_mode","cool_setPoint","heat_setPoint","fan_mode"}
-thermo_props_ro = {"id", "current_temp"}
-operating_mode = {"cool", "heat", "off"}
-fan_mode = {"off", "auto"}
+# Thermostat settings
+thermo_props = {"id", "name", "operating_mode", "cool_setPoint", "heat_setPoint", "fan_mode"}
+thermo_props_ro = {"id",  "current_temp"}
+operating_mode = {"cool",  "heat",  "off"}
+fan_mode = {"off",  "auto"}
 
 
-#Index returns only 200 OK and does not accept any put or post requests
+# Index returns only 200 OK and does not accept any put or post requests
 class Index:
     def GET(self):
-        web.header('Content-Type', 'application/json')
+        web.header('Content-Type',  'application/json')
         raise web.ok(data='200 OK')
 
-#Thermostat that talks with the data
+
+# Thermostat that talks with the data
 class Thermostat:
 
-    #Checks if a value set is valid for the field
-    def check_valid_value(self, thermo_property,setting_value):
+    # Checks if a value set is valid for the field
+    def check_valid_value(self,  thermo_property, setting_value):
         if thermo_property == 'operating_mode' and setting_value in operating_mode:
             return True
         elif thermo_property == 'fan_mode' and setting_value in fan_mode:
@@ -59,15 +60,15 @@ class Thermostat:
 
         return False
 
-    #Returns the specific thermostat queried by id
-    def get_thermostat(self, id):
+    # Returns the specific thermostat queried by id
+    def get_thermostat(self,  id):
         for thermostat in thermostats:
             if int(id) == thermostat['id']:
                 return thermostat
         return None
 
-    def GET(self, id = None, thermo_property = None):
-        web.header('Content-Type', 'application/json')
+    def GET(self, id=None, thermo_property=None):
+        web.header('Content-Type',  'application/json')
         if id is None:
             return json.dumps(thermostats)
 
@@ -80,18 +81,17 @@ class Thermostat:
 
         return json.dumps({thermo_property: info[thermo_property]})
 
-    def PUT(self, id = None, thermo_property = None):
-        web.header('Content-Type', 'application/json')
+    def PUT(self,  id=None,  thermo_property=None):
+        web.header('Content-Type',  'application/json')
         data = web.data()
         json_data = json.loads(data)
 
         if id is None or thermo_property is None:
             raise web.badrequest
 
-        info = self.get_thermostat(id)
         if thermo_property in thermo_props_ro:
             raise web.forbidden
-        if thermo_property not in thermo_props or not self.check_valid_value(thermo_property,json_data):
+        if thermo_property not in thermo_props or not self.check_valid_value(thermo_property, json_data):
             raise web.badrequest()
         for thermostat in thermostats:
             if int(id) == thermostat['id']:
@@ -101,6 +101,6 @@ class Thermostat:
         raise web.notfound()
 
 
-app = web.application(urls, globals())
+app = web.application(urls,  globals())
 if __name__ == "__main__":
     app.run()
